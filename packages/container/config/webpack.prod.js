d7c881c386bc738cf708d45559bb2d459c191aba
Alluminox Devellopers <alluminox.devellopers@gmail.com>
2021-04-08 07:54:00 -0300

    Solving clonflics workxhome

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,13 +8,15 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    // Vamos disponibilizar esse path como publico, ou seja todos, scripts, css teram esse path como prefixo
+    publicPath: '/container/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
       },
       shared: Object.keys(packageJson.dependencies)
     })
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 // Desafio como renderizar a função mount?
+
+const generatedClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 const Typography = () => (
   <div>Home</div>
@@ -14,8 +20,13 @@ const Test = () => (
 
 export default function App() {
   return (
-    <div>
-     <MarketingApp></MarketingApp>
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generatedClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   )
 }
